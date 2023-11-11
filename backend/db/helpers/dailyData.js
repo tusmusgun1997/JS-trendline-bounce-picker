bffb49333f57d542feb4243a4fa781d9ab4f48d2
tusmusgun1997 <tushugunnu@gmail.com>
2023-11-11 14:17:53 +0530

    create-api-for-fetching-filtered-symbols

--- backend/db/helpers/dailyData.js
+++ backend/db/helpers/dailyData.js
@@ -14,8 +14,19 @@ async function fetchDataForSymbol(symbol) {
     throw error;
   }
 }
+async function fetchDistinctSymbolsWithSearchString(searchString) {
+  try {
+    // Use the DailyData model to find distinct symbols that contain the search string
+    const symbols = await DailyData.distinct('symbol', { symbol: { $regex: searchString, $options: 'i' } });
 
+    return symbols;
+  } catch (error) {
+    console.error(`Error fetching distinct symbols with search string ${searchString}: ${error.message}`);
+    throw error;
+  }
+}
 // Export the fetchDataForSymbol function
 module.exports = {
   fetchDataForSymbol,
+  fetchDistinctSymbolsWithSearchString,
 };
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // Importing the express module
 const express = require('express');
 const db = require('./db/config');
-const { fetchDataForSymbol } = require('./db/helpers/dailyData');
+const { fetchDataForSymbol, fetchDistinctSymbolsWithSearchString } = require('./db/helpers/dailyData');
 const { fetchAndSortWeeklyDataForSymbol } = require('./db/helpers/weeklyData');
 
 // Creating an Express application
@@ -30,6 +30,19 @@ app.get('/api/weekly-data/:symbol', async (req, res) => {
     const data = await fetchAndSortWeeklyDataForSymbol(symbol);
 
     res.json({ data });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/api/search-symbols', async (req, res) => {
+  const { search } = req.query;
+
+  try {
+    // Use the helper function to fetch distinct symbols containing the search string
+    const symbols = await fetchDistinctSymbolsWithSearchString(search);
+
+    res.json({ symbols });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
